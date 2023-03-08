16ba321f556fc1d65e6cc3f7479b8700beb48e60
Otto Lima <otto.lima@sou.inteli.edu.br>
2023-03-08 09:10:44 -0300

    perguntas

--- scenes/fase 1/Peguntas/pergunta1.gd
+++ scenes/fase 1/Peguntas/pergunta1.gd
@@ -4,3 +4,4 @@ extends Node2D
 
 func _on_Resposta3_pressed():
 	get_tree().change_scene("res://scenes/fase 1/Peguntas/pergunta2.tscn")
+	Points.addpoint()
--- scenes/fase 1/Peguntas/pergunta2.gd
+++ scenes/fase 1/Peguntas/pergunta2.gd
@@ -2,5 +2,6 @@ extends Node2D
 
 
 
-func _on_Resposta3_pressed():
+func _on_Resposta2_pressed():
 	get_tree().change_scene("res://scenes/fase 1/Peguntas/pergunta3.tscn")
+	Points.addpoint()
