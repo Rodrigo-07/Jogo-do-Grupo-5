72594adb69bcb4a1d80f8f807c8c4efcf5f41b5a
3R11K <124263623+3R11K@users.noreply.github.com>
2023-03-14 13:38:26 -0300

    qr code

---
+++ Pontuação/checkPoints.gd
@@ -0,0 +1,32 @@
+extends Node
+
+var premio = str(Points.points)
+var url = "https://chart.googleapis.com/chart?chs=150x150&cht=qr&chl=Parabens%20voce%20fez%20"+premio+"%20pontos"
+
+func _ready():
+	# Create an HTTP request node and connect its completion signal.
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	http_request.connect("request_completed", self, "_http_request_completed")
+
+	# Perform the HTTP request. The URL below returns a PNG image as of writing.
+	var error = http_request.request(url)
+	if error != OK:
+		push_error("An error occurred in the HTTP request.")
+
+
+# Called when the HTTP request is completed.
+func _http_request_completed(result, response_code, headers, body):
+	var image = Image.new()
+	var error = image.load_png_from_buffer(body)
+	if error != OK:
+		push_error("Couldn't load the image.")
+
+	var texture = ImageTexture.new()
+	texture.create_from_image(image)
+
+	# Display the image in a TextureRect node.
+	var texture_rect = TextureRect.new()
+	add_child(texture_rect)
+	texture_rect.texture = texture
+	texture_rect.set_position(Vector2(100, 200))
