b731ffa34af7611108722e91672f2f77a755d684
Daniel-Zular <daniel.zular@sou.inteli.edu.br>
2023-03-15 15:55:28 -0300

    a

--- scenes/pacman.gd
+++ scenes/pacman.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
-
 func _ready():
-	pass # Replace with function body.
+	pass
+
+
+func _on_Pausar_pressed():
+	get_tree().paused = true
+	$FundoPause.show()
+
+
+func _on_Button2_pressed():
+	get_tree().paused = false
+	$FundoPause.hide()
