bba2bbc6962dae9302cdb731e7591de8633a7995
3R11K <124263623+3R11K@users.noreply.github.com>
2023-03-20 15:42:07 -0300

    Perguntas fase 1, buttons e semi design

--- scenes/fase 1/Peguntas/pergunta4.gd
+++ scenes/fase 1/Peguntas/pergunta4.gd
@@ -4,4 +4,4 @@ func _on_Resposta2_pressed():
 	if Points.pergunta14 == false:
 		Points.addpoint()
 	Points.pergunta14 = true
-	get_tree().change_scene("res://scenes/menu/main_interface.tscn")
+	get_tree().change_scene("res://scenes/fase 1/Peguntas/pergunta4.tscn")
