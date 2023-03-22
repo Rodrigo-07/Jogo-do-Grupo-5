93e6a8b237f8c64dcd6010b2d213bd58045b273d
Rodrigo Sales <freirerodrigosales@gmail.com>
2023-03-22 14:19:15 -0300

    Prototipo dos popup

---
+++ scenes/popup teste.gd
@@ -0,0 +1,14 @@
+extends Popup
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	popup()
+
+func _on_Button_pressed():
+	hide()
