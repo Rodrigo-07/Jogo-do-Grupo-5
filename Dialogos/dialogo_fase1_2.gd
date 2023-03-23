9c0c120d0eda227a17d82d1156efec8679713b29
claramohammad <clara.mohammad@sou.inteli.edu.br>
2023-03-23 20:26:22 -0300

    Troca de sprite Vtal

--- Dialogos/dialogo_fase1_2.gd
+++ Dialogos/dialogo_fase1_2.gd
@@ -12,7 +12,7 @@ var botao = false
 
 func _ready():
 	# Inciar a animação de fala dos personagens
-	$PersonagemVtal/AnimationPlayer.play("trutinha_vtal_falando")
+	$PersonagemVtal/AnimationPlayer.play("boneca_falando")
 	$Alex/AnimationPlayer.play("alex_falando")
 	# Determina velocidaede que o texto aparece na tela
 	$Timer.wait_time = textSpeed
