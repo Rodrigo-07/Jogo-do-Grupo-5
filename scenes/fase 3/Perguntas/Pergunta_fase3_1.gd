fd49788fd5b2c94b98600dd3c9575a3524141375
3R11K <124263623+3R11K@users.noreply.github.com>
2023-03-23 15:55:37 -0300

    Ultimos feedbacks das perguntas

--- scenes/fase 3/Perguntas/Pergunta_fase3_1.gd
+++ scenes/fase 3/Perguntas/Pergunta_fase3_1.gd
@@ -4,32 +4,35 @@ var errou = load("res://sprites/Buttonvermelho.png")
 var acertou = load("res://sprites/Buttonverde.png")
 
 func _process(delta):
-	$Points.text = str(Points.points) + "V Coins"
+	$Points.text = str(Points.points) + " V Coins"
 
 func _on_TextureButton2_button_up():
 	$TextureButton2.texture_normal = errou
 	$TextureButton2.texture_pressed = errou
 	$TextureButton2.texture_hover = errou
 	
-	yield(get_tree().create_timer(1), 'timeout')
-	get_tree().reload_current_scene()
+	yield(get_tree().create_timer(0.5), 'timeout')
+	$FeedbackErro1/Popup.show()
 	
 func _on_TextureButton3_button_up():
 	$TextureButton3.texture_normal = acertou
 	$TextureButton3.texture_pressed = acertou
 	$TextureButton3.texture_hover = acertou
 	
-	yield(get_tree().create_timer(1), 'timeout')
 	if Points.pergunta31 == false:
 		Points.addpoint()
 		Points.addpoint()
 		Points.pergunta31 = true
-	get_tree().change_scene("res://scenes/fase 3/Perguntas/Pergunta_fase3_2.tscn")
+	
+	yield(get_tree().create_timer(0.5), 'timeout')
+	$FeedbackAcerto1/Popup.show()
+	
+	
 
 func _on_TextureButton_button_up():
 	$TextureButton.texture_normal = errou
 	$TextureButton.texture_pressed = errou
 	$TextureButton.texture_hover = errou
 	
-	yield(get_tree().create_timer(1), 'timeout')
-	get_tree().reload_current_scene()
+	yield(get_tree().create_timer(0.5), 'timeout')
+	$FeedbackErro1/Popup.show()
