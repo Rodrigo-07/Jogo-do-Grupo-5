cf5c143293913739245641335d407360e5913b33
samuelnascimento20 <124221367+samuelnascimento20@users.noreply.github.com>
2023-03-28 15:53:56 -0300

    link dos outros jogos

---
+++ linkdosoutrosjogos.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+	
+
+func _on_As_aventuras_de_fibrinha_pressed():
+	OS.shell_open("https://2023m1t8-inteli.github.io/grupo1/")
+
+
+func _on_Vitlicios_pressed():
+	OS.shell_open("https://2023m1t8-inteli.github.io/grupo2/")
+
+func _on_Mr_Vtal_pressed():
+	OS.shell_open("https://2023m1t8-inteli.github.io/grupo3/")
+
+
+func _on_Moraltica_pressed():
+	OS.shell_open("https://2023m1t8-inteli.github.io/grupo4/")
