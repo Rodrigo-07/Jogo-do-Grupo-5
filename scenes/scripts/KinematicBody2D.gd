519e49a4f87e0a28f1b58ade88e1a853805285df
3R11K <124263623+3R11K@users.noreply.github.com>
2023-03-29 09:24:42 -0300

    mudança controle whg

--- scenes/scripts/KinematicBody2D.gd
+++ scenes/scripts/KinematicBody2D.gd
@@ -1,10 +1,13 @@
 extends KinematicBody2D
 
+var player
+
 var move = Vector2().normalized()
+var actualPosition
 var speed = 5000
 var bateu
 var tocou = false
-var keyCard = [0]
+var keyCard = 0
 
 # Variáveis e funções para o funcionamento touch do jogo no mobile
 var cima = false
@@ -15,6 +18,9 @@ var direita = false
 var entrou
 var ganhou
 var nome
+
+func _ready():
+	player = get_node("../KinematicBody2D")
 
 # Funções e variáveis para fazer o movimento do player no celular
 func _on_Esquerda_pressed():
@@ -41,55 +47,21 @@ func _on_Cima_released():
 func _physics_process(delta):
 	
 	# Movimentação do personagem
-	if Input.is_action_pressed("ui_up") or cima:
-		move.y = - speed * delta
-
-	elif Input.is_action_pressed("ui_down") or baixo:
-		move.y = speed * delta
-
-	elif Input.is_action_pressed("ui_right") or direita:
-		move.x = speed * delta
-
-	elif Input.is_action_pressed("ui_left") or esquerda:
-		move.x = - speed * delta
-
-	else:
-		move.x = 0
-		move.y = 0
+	if Input.is_mouse_button_pressed(1):
+		position = position.move_toward(get_global_mouse_position(), 2)
 	
-	move_and_slide(move)
-
-	var collision = move_and_collide(move * delta)
-	var checkCollision = ['TileMap','key', 'key2', 'key3']
-	
-	# Dependendo do que o personagem colidir acontece algo diferente
-	if collision:
-		bateu = collision.collider.name
-		if bateu == 'key' :
-			keyCard[0] += 1
-			get_node("../Camera").modulate = Color(1,1,1,1)
-			get_node("../key/key").queue_free()
-		elif bateu == 'key2':
-			keyCard[0] += 1
-			get_node("../Cell").modulate = Color(1,1,1,1)
-			get_node("../key/key2").queue_free()
-		elif bateu == 'key3':
-			keyCard[0] += 1
-			get_node("../Pasta").modulate = Color(1,1,1,1)
-			get_node("../key/key3").queue_free()
-			
-
-		elif (bateu in checkCollision):
-			
-			print(bateu)
-			
-		elif !(bateu in checkCollision):
-			position = Vector2(0,0)
-			
+		var collision = move_and_collide(move * delta)
+		var checkCollision = ['TileMap','key', 'key2', 'key3']
+		
+		# Dependendo do que o personagem colidir acontece algo diferente
+		if collision:
+			position = Vector2(55,570)
+			print("nhaum", position)
+				
 
 # Quando o player chega no lugar
 func _on_Compliance_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
-	if keyCard == [3]:
+	if keyCard == 3:
 		ganhou = true
 		get_node("../PopUp").show()
 		get_tree().paused = true
@@ -99,8 +71,7 @@ func _on_Compliance_body_shape_entered(body_rid, body, body_shape_index, local_s
 			# Adiciona ponto e seta o estado da fase como true
 			Points.addpoint()
 			Points.whg = true
-		
-
+	
 
 # Quando o jogador vai para o lugar errado
 func _on_RH_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
@@ -127,10 +98,26 @@ func _on_TouchScreenButton_pressed():
 		get_tree().paused = false
 		get_tree().reload_current_scene()
 		
-	if ganhou:
+	if ganhou == true:
 		get_node("../PopUp").hide()
 		get_tree().paused = false
 		get_tree().change_scene("res://scenes/fase 1/Peguntas/pergunta4.tscn")
 
+#Pegando as provas pelo mapa
+func _on_key_body_entered(body):
+	if body == player:
+		get_node("../key/key").queue_free()
+		get_node("../Camera").modulate = Color(1,1,1,1)
+		keyCard += 1
 
+func _on_key2_body_entered(body):
+	if body == player:
+		get_node("../key/key2").queue_free()
+		get_node("../Cell").modulate = Color(1,1,1,1)
+		keyCard += 1
 
+func _on_key3_body_entered(body):
+	if body == player:
+		get_node("../key/key3").queue_free()
+		get_node("../Pasta").modulate = Color(1,1,1,1)
+		keyCard += 1
